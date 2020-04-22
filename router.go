2219d91662274093901e90d2cafb93ef09db989d
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2020-04-22 21:53:11 +1000

    HandlerWrapper now pulls in the args from the request body

--- router.go
+++ router.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
+	"reflect"
 )
 
 // Route used to generate router
@@ -34,12 +36,41 @@ var Routes = []Route{
 	},
 }
 
+// HandlerWrapper implements http.Handler
 type HandlerWrapper struct {
+	// must be a func(ResponseWriter, *Request)
 	F interface{}
 }
 
 func (hw HandlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("wrapper instead of real func")
+	// function itself
+	fn := reflect.ValueOf(hw.F)
+	// function signature
+	sig := fn.Type()
+	if sig.NumIn() < 2 {
+		log.Panicln("inner function has wrong amount of args")
+	}
+	// check that the first two arguments are of the correct type
+	if !(reflect.TypeOf(w).AssignableTo(sig.In(0)) && reflect.TypeOf(r).AssignableTo(sig.In(1))) {
+		log.Panicln("First two args of a function were bad")
+	}
+	argv := make([]reflect.Value, sig.NumIn())
+	argv[0] = reflect.ValueOf(w)
+	argv[1] = reflect.ValueOf(r)
+	// be helpful for the rest of the inut args
+	for i := 2; i < sig.NumIn(); i++ {
+		// fill up a struct arg from the request body
+		if sig.In(i).Kind() == reflect.Struct {
+			arg := reflect.New(sig.In(i)).Interface()
+			defer r.Body.Close()
+			err := json.NewDecoder(r.Body).Decode(&arg)
+			if err != nil {
+				log.Println("Error: ", err.Error())
+			}
+			argv[i] = reflect.ValueOf(arg).Elem()
+		}
+	}
+	fn.Call(argv)
 }
 
 // NewRouter ...
@@ -48,7 +79,7 @@ func NewRouter() *http.ServeMux {
 	for _, route := range Routes {
 		handler := HandlerWrapper{route.HandlerFunc}
 		withMiddleware := requestRouteLogger(handler)
-		withMiddleware = requestIDGenerator(handler)
+		withMiddleware = requestIDGenerator(withMiddleware)
 		mux.Handle(route.Pattern, withMiddleware)
 	}
 	return mux
