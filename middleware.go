b11f4ac13593453ecdfc9f8da20539c4d7a61a5d
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2020-04-26 10:28:38 +1000

    middleware fix

--- middleware.go
+++ middleware.go
@@ -58,7 +58,8 @@ func requestMethodChecker(next http.Handler, cfg RouteConfig) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodOptions {
 			w.Header().Add("Allow", strings.Join(cfg.AllowedMethods, ", "))
-			w.WriteHeader(204)
+			w.WriteHeader(http.StatusNoContent)
+			return
 		}
 		for _, m := range cfg.AllowedMethods {
 			if r.Method == m {
